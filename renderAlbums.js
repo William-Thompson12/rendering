f292d74cd8605f1583747d4ad5891258a5837c74
William Thompson <68480511+Frosty1212@users.noreply.github.com>
2020-10-06 10:52:35 -0400

    still doing it

--- renderAlbums.js
+++ renderAlbums.js
@@ -11,6 +11,9 @@ function renderAlbums(albuminfo) {
         </div>
         <div class="cardBody">
             ${albumCover + albumTitle}
+            <div class="songs>
+                ${albumSongs}
+            </div>
         </div>`
     })
     return albumCard.join(' ')
@@ -20,7 +23,7 @@ function renderAlbumTitle(albums){
     let albumTitle = albums.map(function(albums){
         return`
         <div class="albumTitle">
-            ${albums.title}
+            <h3>${albums.title}<h3>
         </div>`
     })
     return albumTitle.join(' ')
@@ -35,7 +38,12 @@ function renderAlbumSongs(albums) {
     let albumSongs = albumsSongBar.map(function(song){
         return `
         <div class="songBar">
-            ${song.title}${song.length}
+            <a href:"#">
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-play-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+            </svg>${song.title}
+            </a>
+            <p>${song.length}<p>
         </div>`
 
     })
